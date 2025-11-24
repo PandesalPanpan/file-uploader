7cf9426fa4c700ba83a37b3e09fcce0c0ceceef2
PandesalPanpan <petermarticio@gmail.com>
2025-11-24 17:21:22 +0800

    Feat: Make Auth Middleware always attach locals.user

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,6 +1,7 @@
 
 export const isAuth = (req, res, next) => {
     if (req.isAuthenticated()) {
+        res.locals.user= req.user.username;
         next();
     } else {
         res.status(401).redirect('/login')
