88da77dcb13044539513cd4cf4cefcaa8b4e55e5
PandesalPanpan <petermarticio@gmail.com>
2025-11-26 19:53:14 +0800

    Feat: Validate Sign Up

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -2,30 +2,52 @@ import passport from "passport";
 import prisma from "../db/prisma.js";
 import { hashPassword } from "../lib/passwordUtils.js";
 import { isAuth } from "../middlewares/auth.js";
+import { body, validationResult } from "express-validator";
+
+const validateUser = [
+    body("username").trim()
+        .isLength({ min: 6 }).withMessage("Username has to be atleast 6 characters.")
+        .isAlphanumeric().withMessage("Username must be alphanumeric characters only."),
+    body("password")
+        .isLength({ min: 8 }).withMessage("Password must be atleast 8 characters."),
+    body("confirm_password").custom((value, { req }) => {
+        return value === req.body.password
+    }).withMessage("Passwords does not match.")
+]
+
 
 export const signUpGet = (req, res) => {
     res.render("sign-up");
 }
 
-export const signUpPost = async (req, res) => {
-    const { username, password } = req.body;
-
-    const hashedPassword = await hashPassword(password);
-
-    const user = await prisma.user.create({
-        data: {
-            username: username,
-            password: hashedPassword
+export const signUpPost = [
+    validateUser,
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).render("sign-up", {
+                errors: errors.array()
+            });
         }
-    })
 
-    if (!user) {
-        return res.status(400).render("sign-up");
+        const { username, password } = req.body;
+
+        const hashedPassword = await hashPassword(password);
+
+        const user = await prisma.user.create({
+            data: {
+                username: username,
+                password: hashedPassword
+            }
+        })
+
+        if (!user) {
+            return res.status(400).render("sign-up");
+        }
+
+        res.redirect("/login");
     }
-
-    res.redirect("/login");
-
-}
+]
 
 export const loginGet = (req, res) => {
     res.render("login");
